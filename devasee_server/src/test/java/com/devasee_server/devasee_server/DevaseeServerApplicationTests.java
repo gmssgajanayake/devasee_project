6475b489e45fda31a42c50d2e9643d22aa5d3664
G.M.Sakuja Shamal Gajanayake <2021sp026@univ.jfn.ac.lk>
2025-06-11 22:00:21 +0530

    Initial spring boot project

---
+++ devasee_server/src/test/java/com/devasee_server/devasee_server/DevaseeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devasee_server.devasee_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevaseeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
