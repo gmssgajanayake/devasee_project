104a9803bc03b0380e14eceb2389b0b287aa3274
G.M.Sakuja Shamal Gajanayake <2021sp026@univ.jfn.ac.lk>
2025-06-18 22:42:34 +0530

    not found page update

---
+++ devasee_cient/src/app/not-found.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import logo from "@/assets/devasee logo.png";
+
+export default function NotFoundPage(){
+    return (
+        <div className={"w-full h-screen flex flex-col items-center gap-5 justify-center"}>
+            <Image
+                src={logo}
+                alt="Devasee logo"
+                className="w-36 h-auto"
+                priority
+            />
+            <div className={"flex flex-col items-center justify-center"}>
+                <h3 className={"text-2xl font-medium"}>404 Page Not Found </h3>
+            </div>
+
+
+        </div>
+    );
+}
