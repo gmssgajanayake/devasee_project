94d1d44c8506fed183a832f2512eb5901d660b05
G.M.Sakuja Shamal Gajanayake <2021sp026@univ.jfn.ac.lk>
2025-07-02 10:27:00 +0530

    Update layout.tsx

--- devasee_cient/src/app/layout.tsx
+++ devasee_cient/src/app/layout.tsx
@@ -3,8 +3,8 @@ import {Inter} from "next/font/google";
 import "./globals.css";
 import Head from "@/app/head";
 import Footer from "@/app/_components/Footer";
-import MainNavBar from "@/app/_components/MainNavBar";
 import ContactBar from "@/app/_components/ContactBar";
+import MainNavBar from "@/app/_components/MainNavBar";
 
 const inter = Inter({
     subsets: ["latin"],
@@ -51,12 +51,12 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-        <Head/>
-        <body className={`${inter.variable} font-sans antialiased bg-white`}>
-        <ContactBar/>
-        <MainNavBar/>
+        <Head />
+        <body className={`${inter.variable} w-screen overflow-x-hidden font-sans antialiased bg-white`}>
+        <ContactBar />
+        <MainNavBar />
         {children}
-        <Footer/>
+        <Footer />
         </body>
         </html>
     );
