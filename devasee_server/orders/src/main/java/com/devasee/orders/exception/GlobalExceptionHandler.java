05b9fa8221854baeba8e4e68233a2b144e121e4e
K. Nirusha Sandina <2021sp053@univ.jfn.ac.lk>
2025-07-25 08:45:48 +0530

    creating order microservice exception layer

---
+++ devasee_server/orders/src/main/java/com/devasee/orders/exception/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package com.devasee.orders.exception;
+
+import com.devasee.orders.response.CustomResponse;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+
+public class GlobalExceptionHandler {
+}
