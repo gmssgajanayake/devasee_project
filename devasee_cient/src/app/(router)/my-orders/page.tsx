0ca4d98e2fa09a0ca45728ebf4431cdb628f64d3
G.M.Sakuja Shamal Gajanayake <2021sp026@univ.jfn.ac.lk>
2025-07-27 10:51:21 +0530

    add order page

---
+++ devasee_cient/src/app/(router)/my-orders/page.tsx
@@ -0,0 +1,7 @@
+export default function Page(){
+    return(
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h5 className="text-2xl font-bold mb-4">My oders</h5>
+        </div>
+    );
+}
