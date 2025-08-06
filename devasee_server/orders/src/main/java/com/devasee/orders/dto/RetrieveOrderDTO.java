0b07fe8f3157f712f3b39ce006b7e0ef2f447361
K. Nirusha Sandina <2021sp053@univ.jfn.ac.lk>
2025-08-06 16:45:58 +0530

    updating order microservice layer

---
+++ devasee_server/orders/src/main/java/com/devasee/orders/dto/RetrieveOrderDTO.java
@@ -0,0 +1,4 @@
+package com.devasee.orders.dto;
+
+public class RetrieveOrderDTO {
+}
--- devasee_server/orders/src/main/java/com/devasee/orders/exception/GlobalExceptionHandler.java
+++ devasee_server/orders/src/main/java/com/devasee/orders/exception/GlobalExceptionHandler.java
@@ -10,4 +10,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 
 public class GlobalExceptionHandler {
+
+    // Handle any unhandled exceptions
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<CustomResponse<Object>> handleAllExceptions(Exception ex){
+        // Return a generic user-friendly response
+        String message = "Something went wrong. Please try again later.";
+        CustomResponse<Object> response = new CustomResponse<>(false, message, null);
+
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 }
