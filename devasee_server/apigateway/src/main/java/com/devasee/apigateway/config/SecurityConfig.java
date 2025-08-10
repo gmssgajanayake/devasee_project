5f214317634186479b6bb0d3acc2e6ea009450bb
Shashika Pathum Dilhara <161570319+PathumDilhara@users.noreply.github.com>
2025-08-10 18:55:01 +0530

    testing

--- devasee_server/apigateway/src/main/java/com/devasee/apigateway/config/SecurityConfig.java
+++ devasee_server/apigateway/src/main/java/com/devasee/apigateway/config/SecurityConfig.java
@@ -1,11 +1,22 @@
 package com.devasee.apigateway.config;
 
+import com.devasee.apigateway.service.ReactiveUserService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.convert.converter.Converter; // convert JWT into Spring Security auth tokens
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.ServerAuthenticationEntryPoint;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity // This is the reactive version
