01f51dde9592e7e3a2ac0d49aca8ff03080c439c
G.M.Sakuja Shamal Gajanayake <2021sp026@univ.jfn.ac.lk>
2025-09-04 22:23:18 +0530

    dashboard restricted

--- admin-devasee/src/app/page.tsx
+++ admin-devasee/src/app/page.tsx
@@ -4,7 +4,6 @@ import {redirect} from "next/navigation";
 
 export default function LoginPage() {
     useEffect(() => {
-        //redirect("http://localhost:3000/sign-in");
-        redirect("https://admin.devasee.lk/sign-in");
+        redirect("/sign-in");
     }, []);
 }
