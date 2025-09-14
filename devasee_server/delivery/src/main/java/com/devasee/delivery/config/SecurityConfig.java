933168d2c5ee48407ece3a47dec08893b42037bd
ggntheekshana <2021sp273@univ.jfn.ac.lk>
2025-09-14 11:20:05 +0530

    delivery security implementing
    delivery security implementing

---
+++ devasee_server/delivery/src/main/java/com/devasee/delivery/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.devasee.delivery.config;
+
+
+import com.devasee.delivery.filter.InternalJWTFilter;
+import com.devasee.delivery.services.InternalJWTService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    private final InternalJWTService internalJWTService;
+
+    public SecurityConfig(InternalJWTService internalJWTService) {
+        this.internalJWTService = internalJWTService;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                HttpMethod.GET,
+                                "/api/v1/delivery/**",
+                                "/api/v1/delivery"
+                        ).permitAll()
+                        .anyRequest().hasRole("ADMIN"))
+                .addFilterBefore(internalJWTFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public InternalJWTFilter internalJWTFilter() {
+        return new InternalJWTFilter(internalJWTService);
+    }
+}
